8390a713bfae9d8d3f2b6a4697a934f5cdb13cdd
Marshall Clyburn <mardev2013@gmail.com>
2021-11-23 09:08:53 -0500

    Add crash recovery test

---
+++ invariant-validation/20211122-crash-recovery/crash_recovery/src/lib.rs
@@ -0,0 +1,73 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+use std::time::Duration;
+
+#[allow(unused_imports)]
+use clockwise_common::comm::{Direction, Class as SignalClass};
+#[allow(unused_imports)]
+use clockwise_common::{
+    criteria::{
+        Criterion,
+        GPIOCriterion,
+        EnergyCriterion,
+        EnergyStat,
+        Timing,
+        SerialTraceCondition,
+        SerialTraceCriterion,
+    },
+    facility::EnergyMetering,
+    hw::INA219,
+    input::TestProvider,
+    io,
+    io::{
+        Device,
+        Mapping,
+        DeviceInputs,
+    },
+    test::{
+        Operation,
+        Test,
+    },
+};
+
+#[derive(Debug)]
+pub struct SampleTestProvider {
+    tests: Vec<Test>,
+}
+
+impl SampleTestProvider {
+    fn new() -> SampleTestProvider {
+        SampleTestProvider {
+            tests: vec![
+                Test::new(
+                    "crash-recovery",
+                    (&[]).into_iter().copied(),
+                    (&[]).into_iter().copied(),
+                    &[Operation::at(0).idle_sync(Duration::from_millis(3000))],
+                    &[
+                        Criterion::SerialTrace(SerialTraceCriterion::new(&[])),
+                        Criterion::Energy(EnergyCriterion::new("system", EnergyStat::Max)),
+                    ],
+                    true),
+            ]
+        }
+    }
+}
+
+impl TestProvider for SampleTestProvider {
+    fn tests(&self) -> Box<dyn Iterator<Item = Test> + '_> {
+        let it = self.tests.iter()
+            .cloned();
+        Box::new(it)
+    }
+}
+
+#[no_mangle]
+pub fn get_test_adapter() -> Box<dyn TestProvider> {
+    Box::new(SampleTestProvider::new())
+}
